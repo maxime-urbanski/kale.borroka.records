d9c017ad5a007198be97a5ccc0f07a71ca41e608
Maxiloud <maxime.urbanski@gmail.com>
2020-06-21 12:18:52 +0200

    add CD article

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -5,6 +5,7 @@ import MenuNav from "./NavBar";
 import Home from "./home";
 import LP from "./LpList";
 import EpList from "./EpList";
+import cd from "./CD";
 import divers from "./Divers";
 import VinyleDetails from "./VinyleDetails";
 
@@ -12,6 +13,7 @@ import "./menu.css";
 import HowBuy from "./HowBuy";
 import ContactUs from "./ContactUs";
 import Crew from "./Crew";
+import cdList from "./cdList";
 
 function Router() {
     return (
@@ -20,16 +22,18 @@ function Router() {
             <div className="mt-5 ban">              
             </div>
             <Switch>
-                <Route path="/LP/:id" component={VinyleDetails} />
+                <Route path="/CD:page" component={cdList} />
                 <Route path="/LP:page" component={LP} />
                 <Route path="/EP:page" component={EpList} />
+                <Route path="/LP/:id" component={VinyleDetails} />
                 <Route path="/EP/:id" component={VinyleDetails} />
+                <Route path="/CD/:id" component={VinyleDetails} />
                 <Route exact path="/" component={Home} />
                 <Route exact path="/crew" component={Crew} />
                 <Route exact path="/contact" component={ContactUs} />
                 <Route exact path="/how-to-buy" component={HowBuy} />
                 <Route path="/LP" component={LP} />
-                <Route path="/CD" component={""} />
+                <Route path="/CD" component={cdList} />
                 <Route path="/EP" component={EpList} />
                 <Route path="/Production" component={""} />
                 <Route path="/Divers" component={divers} />
