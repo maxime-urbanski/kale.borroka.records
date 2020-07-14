62c4266bf67a1c7ba9260e51c817766b5c4068a5
Maxiloud <maxime.urbanski@gmail.com>
2020-07-14 10:16:36 +0200

    try to resolved bug in netlify

--- src/components/PageShop/fanzineList.jsx
+++ src/components/PageShop/fanzineList.jsx
@@ -11,42 +11,46 @@ import Footer from "../Footer/Footer";
 import fanzine from "./marchandise/Fanzine";
 
 class fanzineList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentPage: "1",
-            cardsPerPage: "9",
-        };
-    }
-    render() {
-        return (
-            <>
-                <Container>
-                    <Breadcrumb listTag="div">
-                        <BreadcrumbItem tag={Link} to={"/"}>
-                            KALE BORROKA RECORDS
-                        </BreadcrumbItem>
-                        <BreadcrumbItem tag={Link} to={"/divers"}>
-                            DIVERS
-                        </BreadcrumbItem>
-                        <BreadcrumbItem tactive tag="span">
-                            FANZINE
-                        </BreadcrumbItem>
-                    </Breadcrumb>
-                    <Col>
-                        <Row className="position">
-                            <h1 className="mt-3 position">Tous nos Fanzines</h1>
-                        </Row>
-                        <Row className="position">
-                            {currentCards.map((item) => {
-                                return item.quantity > 0 ? (
-                                    <CardArticle {...item} key={item.id} />
-                                ) : (
-                                    ""
-                                );
-                            })}
-                        </Row>
-                        {/* <Row>
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: "1",
+      cardsPerPage: "9",
+    };
+  }
+  render() {
+    const { currentPage, cardsPerPage } = this.state;
+    const indexLastCards = currentPage * cardsPerPage;
+    const indexFirstCards = indexLastCards - cardsPerPage;
+    const currentCards = fanzine.slice(indexFirstCards, indexLastCards);
+    return (
+      <>
+        <Container>
+          <Breadcrumb listTag="div">
+            <BreadcrumbItem tag={Link} to={"/"}>
+              KALE BORROKA RECORDS
+            </BreadcrumbItem>
+            <BreadcrumbItem tag={Link} to={"/divers"}>
+              DIVERS
+            </BreadcrumbItem>
+            <BreadcrumbItem tactive tag="span">
+              FANZINE
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <Col>
+            <Row className="position">
+              <h1 className="mt-3 position">Tous nos Fanzines</h1>
+            </Row>
+            <Row className="position">
+              {currentCards.map((item) => {
+                return item.quantity > 0 ? (
+                  <CardArticle {...item} key={item.id} />
+                ) : (
+                  ""
+                );
+              })}
+            </Row>
+            {/* <Row>
                             <PaginationDistro
                                 cardsPerPage={cardsPerPage}
                                 totalCards={fanzineList}
@@ -54,11 +58,11 @@ class fanzineList extends React.Component {
                                 format={format}
                             />
                         </Row> */}
-                    </Col>
-                </Container>
-                <Footer />
-            </>
-        );
-    }
+          </Col>
+        </Container>
+        <Footer />
+      </>
+    );
+  }
 }
 export default fanzineList;
