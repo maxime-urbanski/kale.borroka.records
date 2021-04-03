c4034c7e4daa55c3976aeff66a60c7439b19daf1
Urbanski Maxime <maxime@les-tilleuls.coop>
2021-04-03 22:42:54 +0200

    Added <head> and typescript

---
+++ kbr_front/pages/_app.tsx
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="author" content="Maxiloud" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="application-name" content="Kale Borroka Records" />
+        <meta name="keywords" content="Punk, Oi!, HardxCore, Skin, antifa/" />
+        <meta
+          name="description"
+          content="Kale Borroka Records, Label &amp; distro from Baiona, Euskal Herria. Shop Music."
+        />
+        <title>
+          Kale Borroka Records | Antifascit distro &amp; label from Baiona,
+          Euskal Herria !
+        </title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
--- kbr_front/pages/index.js
+++ kbr_front/pages/index.js
@@ -1,21 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +51,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
