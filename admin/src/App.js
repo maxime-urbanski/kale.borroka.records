d35f55c9134d92aebdd988534571f2be5b14ea53
Urbanski Maxime <maxiloud@gmail.com>
2021-11-08 15:55:11 +0100

    feature: created dockerfile and docker-compose.yml

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,15 +4,17 @@ import authProvider from './components/auth/auth';
 import { resources } from './components/data';
 import { CustomDataProvider } from './components/auth/CustomDataProvider';
 
-const App = () => (
-  <>
-    <Admin dataProvider={CustomDataProvider} authProvider={authProvider}>
-      {resources.map(({
-        name, list, edit, create, icon,
-      }, index) => (
-        <Resource name={name} list={list} edit={edit} create={create} icon={icon} key={index} />
-      ))}
-    </Admin>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <Admin dataProvider={CustomDataProvider} authProvider={authProvider}>
+        {resources.map(({
+                          name, list, edit, create, icon,
+                        }, index) => (
+          <Resource name={name} list={list} edit={edit} create={create} icon={icon} key={index} />
+        ))}
+      </Admin>
+    </>
+  );
+}
 export default App;
--- admin/src/components/auth/CustomDataProvider.js
+++ admin/src/components/auth/CustomDataProvider.js
@@ -1,4 +1,3 @@
-import { stringify } from 'querystring';
 import axios from 'axios';
 
 const apiUrl = process.env.REACT_APP_BACK_URL;
